b6a86c0ff93fa151e477c7a6b22f533648d900dd
frostime <frostime@foxmail.com>
2024-10-09 22:55:23 +0800

    🐛 fix: update

--- src/signal-ref.ts
+++ src/signal-ref.ts
@@ -3,7 +3,7 @@
  * @Author       : frostime
  * @Date         : 2024-10-09 20:32:37
  * @FilePath     : /src/signal-ref.ts
- * @LastEditTime : 2024-10-09 21:59:40
+ * @LastEditTime : 2024-10-09 22:53:43
  * @Description  : 
  */
 import { createSignal, type Setter } from "solid-js";
@@ -12,30 +12,12 @@ interface SignalRef<T> {
     (): T;
     (value: T): T;
     value: T;
-    update: (value: Parameters<Setter<T>>[0]) => void;
+    update: Setter<T>;
 }
 
 /**
- * 创建一个类似 Vue ref 的响应式信号，基于 SolidJS 的 createSignal
- * 
- * 这个函数返回一个对象，可以通过函数调用或 .value 属性来获取和设置值。
- * 它结合了 SolidJS 的响应式特性和类似 Vue ref 的使用方式。
- * 
- * 用法:
-    ```js
-    const count = useSignal(0);
-
-    // 函数调用方式
-    console.log(count()); // 获取值
-    count(1); // 设置值
-
-    // .value 属性方式
-    console.log(count.value); // 获取值
-    count.value = 2; // 设置值
-    ```
- * 
- * @param initialValue - 信号的初始值
- * @returns 一个 RefSignal 对象，可以通过函数调用或 .value 属性来操作
+ * @param initialValue
+ * @returns RefSignal
  */
 const useSignalRef = <T>(initialValue: T): SignalRef<T> => {
     const [signal, setSignal] = createSignal<T>(initialValue);
@@ -53,7 +35,7 @@ const useSignalRef = <T>(initialValue: T): SignalRef<T> => {
         set: (param: Parameters<typeof setSignal>[0]) => setSignal(param),
     });
     Object.defineProperty(refSignal, 'update', {
-        value: (param: Parameters<typeof setSignal>[0]) => setSignal(param),
+        value: setSignal,
         writable: false,
     })
 
